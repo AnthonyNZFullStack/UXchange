fbe1647b71ac80449f7636f65251c50a63f0756e
AnthonyFullStack <62530716+AnthonyFullStack@users.noreply.github.com>
2021-08-17 15:30:57 +1200

    MyItemsCard Array

--- uxchange/src/Components/ItemCard/MyItemCard.tsx
+++ uxchange/src/Components/ItemCard/MyItemCard.tsx
@@ -30,7 +30,6 @@ const useStyles = makeStyles((theme: Theme) =>
     })
 );
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 interface Props {
     image: string;
@@ -39,46 +38,42 @@ interface Props {
 }
 
 
-function MyItemCard() {
+function MyItemCard({ image, title, description }: Props) {
     const classes = useStyles();
 
     return (
         <React.Fragment>
             <CssBaseline />
 
-            <Container className={classes.cardGrid} maxWidth="md">
-                <Grid container spacing={4}>
-                    {cards.map((card) => (
-                        <Grid item key={card} xs={12} sm={6} md={4}>
-                            <Card className={classes.card}>
-                                <CardMedia
-                                    className={classes.cardMedia}
-                                    image="https://waihekebikeshop.co.nz/wp-content/uploads/2020/05/cropped-gabe_7R32545_pdd002_2000.jpg"
-                                    title="Second hand bike"
-                                />
-                                <CardContent className={classes.cardContent}>
-                                    <Typography gutterBottom variant="h5">
-                                        Second hand Bike
-                                    </Typography>
-                                    <Typography variant="body2" component="p">
-                                        This is a media card
-                                    </Typography>
-                                </CardContent>
-                                <CardActions style={{textAlign: 'right'}}>
-                                    <Button size="small" color="primary">
-                                        <Typography> <CreateIcon />edit </Typography>
-                                    </Button>
-                                    <Button size="small" color="primary">
-                                        <DeleteIcon />
-                                        delete
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                    ))}
+            <Container className={classes.cardGrid} maxWidth="sm">
+                    <Grid>
+                        <Card className={classes.card}>
+                            <CardMedia
+                                className={classes.cardMedia}
+                                image={image}
+                                title={title}
+                            />
+                            <CardContent className={classes.cardContent}>
+                                <Typography gutterBottom variant="h5">
+                                    {title}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                    {description}
+                                </Typography>
+                            </CardContent>
+                            <CardActions style={{ textAlign: 'right' }}>
+                                <Button size="small" color="primary">
+                                    <Typography> <CreateIcon />edit </Typography>
+                                </Button>
+                                <Button size="small" color="primary">
+                                    <DeleteIcon />
+                                    delete
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
 
 
-                </Grid>
             </Container>
 
 
--- uxchange/src/Pages/MyItems/MyItems.tsx
+++ uxchange/src/Pages/MyItems/MyItems.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { Typography, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Container } from "@material-ui/core";
+import { Typography, Button, CssBaseline, Grid, Container } from "@material-ui/core";
 import { Theme } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import createStyles from "@material-ui/core/styles/createStyles";
 import MyItemCard from "../../Components/ItemCard/MyItemCard";
 import { Link } from 'react-router-dom';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Box from '@material-ui/core/Box';
+
 
 
 
@@ -39,14 +41,68 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const myItemCard = [
+  {
+    image: 'https://waihekebikeshop.co.nz/wp-content/uploads/2020/05/cropped-gabe_7R32545_pdd002_2000.jpg',
+    title: '1) Second hand bike',
+    description: 'The best bike there is',
+  },
+
+  {
+    image: 'https://cdn.shopify.com/s/files/1/2647/0684/files/kaabo-mantis-dual-full__91784.1588047382.1280.1280_large.jpg?v=1596175027',
+    title: '2) E-Scooter',
+    description: 'The best bike there is Lorem Ipsum is simply dummy e industryy kjbjkkjhldffddffdgfddf',
+  },
+
+  {
+    image: 'https://img3.exportersindia.com/product_images/bc-full/2019/2/6148197/apple-iphone-xs-max-512gb-1549643543-4706321.jpeg',
+    title: '3) Iphone XS',
+    description: 'Brand new Iphone for sale',
+  },
+
+  {
+    image: 'https://waihekebikeshop.co.nz/wp-content/uploads/2020/05/cropped-gabe_7R32545_pdd002_2000.jpg',
+    title: '4) Second hand bike',
+    description: 'Testing Testing testing',
+  },
+
+  {
+    image: 'https://waihekebikeshop.co.nz/wp-content/uploads/2020/05/cropped-gabe_7R32545_pdd002_2000.jpg',
+    title: '5) Second hand bike',
+    description: 'Testing Testing testing',
+  },
+
+  {
+    image: 'https://waihekebikeshop.co.nz/wp-content/uploads/2020/05/cropped-gabe_7R32545_pdd002_2000.jpg',
+    title: '6) Second hand bike',
+    description: 'Testing Testing testing',
+  },
+
+  {
+    image: 'https://waihekebikeshop.co.nz/wp-content/uploads/2020/05/cropped-gabe_7R32545_pdd002_2000.jpg',
+    title: '7) Second hand bike',
+    description: 'Testing Testing testing',
+  },
+
+  {
+    image: 'https://waihekebikeshop.co.nz/wp-content/uploads/2020/05/cropped-gabe_7R32545_pdd002_2000.jpg',
+    title: '8) Second hand bike',
+    description: 'Testing Testing testing',
+  },
+
+  {
+    image: 'https://waihekebikeshop.co.nz/wp-content/uploads/2020/05/cropped-gabe_7R32545_pdd002_2000.jpg',
+    title: '9) Second hand bike',
+    description: 'Testing Testing testing',
+  },
+];
 
 
 function MyItems() {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
+    < React.Fragment >
       <CssBaseline />
 
       <main>
@@ -57,14 +113,14 @@ function MyItems() {
               My Item List
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph
-            style={{paddingBottom: '2em'}}>
+              style={{ paddingBottom: '2em' }}>
               Create your post, update & delete
             </Typography>
             <div className={classes.buttons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
                   <Button variant="outlined" color="primary"
-                  component={Link} to="/Market Place">
+                    component={Link} to="/Market Place">
                     Market items
                   </Button>
                 </Grid>
@@ -75,13 +131,25 @@ function MyItems() {
                 </Grid>
                 <Grid item>
                   <Button variant="outlined" color="primary">
-                   <AddCircleIcon style={{marginRight:'0.2em'}}></AddCircleIcon> Create I-post
+                    <AddCircleIcon style={{ marginRight: '0.2em' }}></AddCircleIcon> Create I-post
                   </Button>
                 </Grid>
               </Grid>
             </div>
           </Container>
-          <MyItemCard />
+
+          <Box p={5} m={5}>
+            <Grid container spacing={10}>
+              {myItemCard.map((myItemCard, i) => {
+                return (
+                  <Grid key={i} item xs={12} sm={6} md={4} xl={3}>
+                    <MyItemCard {...myItemCard} />
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Box>
+
         </div>
       </main>
 
